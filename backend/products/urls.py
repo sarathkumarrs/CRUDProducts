940c62241950c55fe7acc49396c50c45166d1c79
Sooraj A R <soorajar31@gmail.com>
2023-03-02 15:26:26 +0530

    update

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     
     path('products/',ProductsListView.as_view(),name='products-list'),
     path('add/product/',ProductsView.as_view(),name='add-product'),
-     path('filter/products/',FilterProduct.as_view(),name='filter-product')
+    path('filter/products/',FilterProduct.as_view(),name='filter-product')
 ]
--- backend/products/views.py
+++ backend/products/views.py
@@ -10,7 +10,7 @@ from rest_framework import status
 
 # Create your views here.
 class Pagination(PageNumberPagination):
-    page_size = 30
+    page_size = 3
     page_size_query_param = 'page_size'
     max_page_size = 1000
 
